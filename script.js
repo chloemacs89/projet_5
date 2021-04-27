0f06f9e75364204256784e4c9975d2128cc49e89
Jeremy Villey <jer.villey@gmail.com>
2021-04-27 23:33:06 +0200

    Added modal mechanism and displays

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Carousel control
+
 const gap = 5;
 
 const next = document.querySelectorAll(".next"),
@@ -9,7 +11,7 @@ for (var i = 0; i < next.length; i++) {
     let width = crl.offsetWidth;
     next[i].onclick = function(e) {
 	crl.scrollBy(width + gap, 0);
-    }
+    };
 }
 
 for (var i = 0; i < prev.length; i++) {
@@ -17,7 +19,30 @@ for (var i = 0; i < prev.length; i++) {
     let width = crl.offsetWidth;
     prev[i].onclick = function(e) {
 	crl.scrollBy(-(width + gap), 0);
-    }
+    };
 }
+
+// Modal control
+
+var modal = document.getElementById("myModal");
+const descriptionBtn = document.querySelectorAll(".description");
+var closeBtn = document.getElementsByClassName("close")[0];
+
+for (var j = 0; j < descriptionBtn.length; j++) {
+    descriptionBtn[j].onclick = function(e) {
+	modal.style.display = "block";
+    };
+}
+
+closeBtn.onclick = function(e) {
+    modal.style.display = "none";
+};
+
+
+window.onclick = function(e) {
+    if (e.target == modal) {
+	modal.style.display = "none";
+    }
+};
 
 
